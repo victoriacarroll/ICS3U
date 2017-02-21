b9cf36a497bc28a6200959994bd65d98fbd85690
Victoria Carroll <323966481@C-4028YR1.tdsb.on.ca>
2017-02-21 14:47:38 -0500

    Pushing HelloWorld

---
+++ Hello/src/carroll/HelloWorld.java
@@ -0,0 +1,16 @@
+package carroll;
+/**
+ * HelloWorld.java
+ *Program prints swag
+ *2017/02/21
+ * @author Victoria Carroll
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("swag");
+
+	}
+
+}
